d76a0b72ebec0585a5ef033d7c713208a52b8603
yasmimvalduga <163342034+yasmimvalduga@users.noreply.github.com>
2024-08-07 16:18:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -88,24 +88,41 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+      }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
+
 
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
       const botaoAlternativas = document.createElement("button");
       botaoAlternativas.textContent = alternativa.texto;
-      botaoAlternativas.addEventListener("click", function () {
-        atual++;
-        mostraPergunta();
-      });
+      botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+
       caixaAlternativas.appendChild(botaoAlternativas);
     }
   }
   
+  function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+   }
+   
+   function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
 
 mostraPergunta()
